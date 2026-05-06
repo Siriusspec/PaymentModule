999f079e754c90e73ad3fe8b32f77e0ec7da4f5c
Siriusspec <202994199+Siriusspec@users.noreply.github.com>
2026-05-06 13:01:33 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -3,18 +3,13 @@ from sqlalchemy.orm import sessionmaker
 from database.models import Base
 import os
 
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
-
-
+# Fix for Render's postgres:// URL issue
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-
-if "sqlite" in DATABASE_URL:
-    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-else:
-    engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine)
 
